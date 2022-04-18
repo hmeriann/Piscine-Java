514ba8828a3582c3e10cae2b94ee438024c39dbd
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2022-04-18 22:53:33 +0300

    ex01

--- Module_00/ex01/Program.java
+++ Module_00/ex01/Program.java
@@ -4,13 +4,47 @@ public class Program{
 	public static void main (String args[]){
 		Scanner	number = new Scanner(System.in);
 		System.out.print("->");
-		Integer num = number.nextInt();
 
-		if (num.isEmpty())
+		int	num = number.nextInt();
+		boolean simple = true;
+//		int	count = 0;
+		int	i = 2;
+
+		if (num <= 1)
 		{
-			System.err.print("IllegalArgument");
-//			System.exit(1);
+			System.err.println("IllegalArgument");
+			System.exit(-1);
+		} else if (num == 2 || num == 3) {
+//			count++;
+			System.out.print(simple + " " + (i - 1) + "\n");
+		} else
+		{
+			while (i <= ft_sqrt(num))
+			{
+				if (num % i == 0) {
+					simple = false;
+					break;
+				}
+				i++;
+//				count++;
+			}
+			if (simple)
+				System.out.print(simple + " " + (ft_sqrt(num) - 1) + "\n");
+			else
+				System.out.print(simple + " " + (ft_sqrt(num) - 1) + "\n");
 		}
-		System.out.println(number);
+	}
+
+	private static int	ft_sqrt(int num)
+	{
+		int i = 1;
+		int res = 0;
+		while (num > 0)
+		{
+			num = num - i;
+			i += 2;
+			res++;
+		}
+		return (res);
 	}
 }
