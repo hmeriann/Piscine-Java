f894a4a682b0371a9abd68f6402837b7652eff79
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2022-04-22 22:19:47 +0300

    ugh - 03 is done

--- Module_03/ex02/EachThread.java
+++ Module_03/ex02/EachThread.java
@@ -1,33 +1,35 @@
 public class EachThread implements Runnable {
     int from;
     int to;
-    int sectionSize;
+    int[] sum;
     int[] randomArray;
     int threadsCount;
-    int sumByThread;
+    Object lock;
 
-    EachThread(int from, int to, int sectionSize, int[] randomArray, int threadsCount) {
+    EachThread(int from, int to, int[] sum, int[] randomArray, int threadsCount, Object lock) {
         this.from = from;
         this.to = to;
-        this.sectionSize = sectionSize;
+        this.sum = sum;
         this.randomArray = randomArray;
         this.threadsCount = threadsCount;
+        this.lock = lock;
     }
-
 
     @Override
     public void run() {
-        sumByThread = Program.getSumEachThread(from, to, randomArray);
-        System.out.print("Thread " + threadsCount + " from ");
-        System.out.println(from + " to " + to + " sum is " + sumByThread);
-//
-//        for (int i = 0; i < (i * threadsCount + this.sectionSize); i++) {
-//            sumByThread = sumByThread + randomArray[i];
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
+        int startSum = this.sum[0];
+        int startFrom = this.from;
+        for (; this.from < this.to; this.from++) {
+            synchronized (lock) {
+                this.sum[0] +=this.randomArray[this.from];
+            }
+        }
+        System.out.print("Thread " + (this.threadsCount + 1) + " from " + startFrom);
+        if (this.to != (this.randomArray.length - 1)) {
+            System.out.println(" to " + (this.to - 1) + " sum is " + (this.sum[0] - startSum));
+        } else {
+            System.out.println(" to " + (this.to) + " sum is " + (this.sum[0] - startSum));
+        }
     }
+
 }
--- Module_03/ex02/Program.java
+++ Module_03/ex02/Program.java
@@ -1,21 +1,14 @@
 import java.util.Random;
 
 public class Program {
-
-    private int sumInThread;
-
-    Program() { this.sumInThread = 0; }
-
-    int getSumInThread() { return this.sumInThread; }
-
-
     public static void main(String[] args) throws InterruptedException {
         int arrSize;
         int threadsCount;
-        int sectionSize;
+        int[] sum = new int[1];
+        int sectionSize = 0;
         int sumArrayElements = 0;
-        int sumByAllThreads = 0;
         int[] randomArray;
+        Object lock = new Object();
 
         if (args.length != 2) {
             System.out.println("Wrong count of arguments\nUsage: \"--arraySize=<number of array elements : 0..2_000_000> --threadsCount=<threads count>\"");
@@ -30,33 +23,42 @@ public class Program {
             System.out.println("Wrong array size\nUsage: \"--arraySize=<number of array elements : 0..2_000_000> --threadsCount=<threads count>\"");
             System.exit(1);
         }
+        threadsCount = getCount(args[1]);
+        if (arrSize < threadsCount){
+            System.out.println("Wrong threads count\nThreads count should be less than array size\"");
+            System.exit(1);
+        }
 
         randomArray = new int[arrSize];
         for (int i = 0; i < arrSize; i++)
         {
-//            randomArray[i] = new Random().nextInt((1_001 - (-1_000))) + (-1_000);
             randomArray[i] = (int) Math.round((Math.random() * 2000) - 1000);
-            System.out.print(randomArray[i]+ "->");
-            sumArrayElements += i;
+            sumArrayElements += randomArray[i];
         }
         System.out.println("Sum: " + sumArrayElements);
 
-        threadsCount = getCount(args[1]);
-        sectionSize = arrSize / threadsCount;
+        sectionSize = (arrSize / (threadsCount - 1));
 
-        for (int i = 0; i < (threadsCount - 1); i++)
+        for (int i = 0; i < (threadsCount); i++)
         {
-            int from = i * (sectionSize - 1);
-            int to = i + (sectionSize - 1);
-            Thread eachThread = new Thread(new EachThread(from, to, sectionSize, randomArray, i));
-            eachThread.start();
-//            sumByAllThreads = sumByAllThreads;
-            eachThread.join();
+            int from = i * sectionSize;
+            int to = (i + 1) * sectionSize;
+
+            if (i == (threadsCount - 1)) {
+                to = arrSize;
+            }
+
+            Thread eachThread = new Thread(new EachThread(from, to, sum, randomArray, i, lock));
+
+            try {
+                eachThread.start();
+                eachThread.join();
+            } catch (InterruptedException e) {
+                System.err.println(e.getStackTrace());
+                System.exit(1);
+            }
         }
-        Thread lastThread = new Thread(new EachThread((sectionSize*(threadsCount - 1)), (arrSize - 1), sectionSize, randomArray, threadsCount));
-        lastThread.start();
-        lastThread.join();
-        System.out.println("Sum by threads: " + sumByAllThreads);
+        System.out.println("Sum by threads: " + sum[0]);
 
     }
 
@@ -77,18 +79,4 @@ public class Program {
         return (i);
     }
 
-    public static int getSumEachThread(int from, int to, int[] randomArray) {
-        int sum = 0;
-
-        for (int i = from; i < to; i++)
-            sum += randomArray[i];
-        return sum;
-    }
-
-//    public static int getTotalSum() {
-//        int total = 0;
-//
-//
-//        return total;
-//    }
 }
