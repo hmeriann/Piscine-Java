2f541c221bf98a9919edebc47f6f67f1103f83e4
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2022-04-27 13:49:54 +0300

    ex00 dday 06

---
+++ Module_06/src/main/java/edu/school21/numbers/NumberWorker.java
@@ -0,0 +1,34 @@
+package edu.school21.numbers;
+
+public class NumberWorker {
+
+    public int digitsSum(int number) {
+        int res = 0;
+
+        while (number > 0)
+        {
+            res = res + (number % 10);
+            number = number / 10;
+        }
+        return res;
+    }
+
+    public boolean isPrime(int number) throws IllegalNumberException {
+        int	i = 2;
+
+        if (number <= 1) {
+            throw new IllegalNumberException("Number less than 2 is passed to the program");
+        }
+        if (number == 2 || number == 3) {
+            return true;
+        }
+        int res = 0;
+        while (res < number) {
+            res = i * i;
+            if (number % i == 0)
+                return false;
+            i++;
+        }
+        return true;
+    }
+}
---
+++ Module_06/src/test/java/edu/school21/numbers/NumberWorkerTest.java
@@ -0,0 +1,47 @@
+package edu.school21.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NumberWorkerTest {
+    NumberWorker numberWorker;
+
+    @BeforeEach
+    void beforeEach() { numberWorker = new NumberWorker(); }
+
+    @ParameterizedTest(name = "{index} - {0} is a prime")
+    @ValueSource(ints = {2147483629, 113, 2, 3, 5, 7, 11, 13, 17, 19, 23})
+    void isPrimeForPrimes(int number) {
+        try {
+            Assertions.assertTrue(numberWorker.isPrime(number));
+        } catch (IllegalNumberException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @ParameterizedTest(name = "{index} - {0} is not a prime")
+    @ValueSource(ints = {2147483628, 112, 6, 42, 4, 16, 75})
+    void isPrimeForNotPrimes(int number) {
+        try {
+            Assertions.assertFalse(numberWorker.isPrime(number));
+        } catch (IllegalNumberException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @ParameterizedTest(name = "{index} - {0} is illegal")
+    @ValueSource(ints = {1, 0, -2549, -42, -1, -2147483627})
+    void isPrimeForIncorrectNumbers(int number) {
+        Assertions.assertThrows(IllegalNumberException.class, ()-> numberWorker.isPrime(number));
+    }
+
+    @ParameterizedTest(name = "{index} - digits sum of number {0} is equal to {1}")
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    void checkDigitsSum(int number, int res) {
+        Assertions.assertEquals(numberWorker.digitsSum(number), res);
+    }
+//4. a method to check digitsSum using a set of at least 10 numbers
+}
