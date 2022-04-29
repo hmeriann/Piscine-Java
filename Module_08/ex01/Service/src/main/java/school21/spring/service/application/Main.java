3634a7c7e36868cf881ca091e08c641ffdc3620d
Heidi Merianne <hmeriann@un-g6.kzn.21-school.ru>
2022-04-29 21:07:57 +0300

    ex01

--- Module_08/ex01/Service/src/main/java/school21/spring/service/application/Main.java
+++ Module_08/ex01/Service/src/main/java/school21/spring/service/application/Main.java
@@ -11,6 +11,8 @@ public class Main   {
 	public static void main(String[] args) throws SQLException {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+
+		System.out.println("\nusersRepositoryJdbc");
 		UsersRepository usersRepository = context.getBean("usersRepositoryJdbc", UsersRepository.class);
 		System.out.println(usersRepository.findAll());
 		System.out.println(usersRepository.findById(4L));
@@ -21,13 +23,26 @@ public class Main   {
 		System.out.println(usersRepository.findAll());
 		usersRepository.update(newUser);
 		System.out.println(usersRepository.findAll());
-		usersRepository.delete(9L);
+		usersRepository.delete(1L);
+
+		System.out.println("\nUsersRepositoryJdbcTemplate");
+
 		System.out.println(usersRepository.findAll());
 		System.out.println(usersRepository.findByEmail("maymay@mail.ru"));
 
-//		usersRepository = context.getBean("usersRepositoryJdbcTemplate", UsersRepository.class);
-//		System.out.println(usersRepository.findAll());
+		usersRepository = context.getBean("UsersRepositoryJdbcTemplate", UsersRepository.class);
+		System.out.println(usersRepository.findAll());
+		System.out.println(usersRepository.findById(4L));
 
+		User new2User = new User("byebye@mail.ru");
+
+		usersRepository.save(new2User);
+		System.out.println(usersRepository.findAll());
+		usersRepository.update(newUser);
+		System.out.println(usersRepository.findAll());
+		usersRepository.delete(1L);
+		System.out.println(usersRepository.findAll());
+		System.out.println(usersRepository.findByEmail("byebye@mail.ru"));
 	}
 
 
